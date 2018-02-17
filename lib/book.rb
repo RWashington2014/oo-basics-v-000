80701561abd09ce4e94014d41c83bd801637bc1b
Robert W <robwash@gmail.com>
2018-02-17 05:25:57 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,9 +10,16 @@ class Book
   def author= (author)
     @author = author
   end
-  
+
   def author
     @author
+  end
+
+  def page_count= (page_count)
+    @page_count = page_count
+  end
+  def page_count
+    @page_count  
   end
 
 end
