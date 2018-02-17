87155a17130aa2bfd24891021b1a2ecaa1b9a14a
Robert W <robwash@gmail.com>
2018-02-17 05:27:53 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -15,8 +15,8 @@ class Book
     @author
   end
 
-  def page_count= (page_count)
-    @page_count = page_count
+  def page_count= (num)
+    @page_count = num
   end
 
   def page_count
@@ -28,7 +28,7 @@ class Book
   end
 
   def genre
-    @genre    
+    @genre
   end
 
 end
